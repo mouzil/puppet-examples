cc4c95fdda952c9263bddcf5a90ac2976e771734
mouzilÃ <mouzil@examples.com>
2017-01-09 14:42:28 +0000

    teste

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,4 +2,11 @@ node 'agent1' {
 	file { '/tmp/hello' :
 	   content => "Hello, world\n",
 	}
-} 
+}
+
+node 'agent2' {
+        file { '/tmp/hello' :
+           content => "Hello, world\n",
+        }
+}
+ 
